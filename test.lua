f5f475edb9e728208b88b47e91048c2095ac620f
Alex Kramer <amkramer2010@yahoo.com>
2019-05-14 20:01:31 -0700

    edit example

--- test.lua
+++ test.lua
@@ -1,6 +1,8 @@
 require "src/HMM"
 
-HMM.load("example/2nd_order_hmm.txt")
+--HMM.load("example/2nd_order_hmm.txt")
+HMM.load("example/1st_order_hmm.txt")
+
 
 emitted_symbols, emitted_states = HMM.emit(1000)
 decoded_states = HMM.decode(emitted_symbols)
